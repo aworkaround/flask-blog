9f95bb0bfd535238ca7f580eb840bad89ab38a54
kamal Kumar <kamal.kumar9@outlook.com>
2024-01-27 13:38:13 +0530

    Customize the template

--- main.py
+++ main.py
@@ -2,15 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+blogs = [
+    {
+        "title": "This is a Blog 1",
+        "subtitle": "This is subtitle of the blog",
+        "description": "This is the description of my new blog",
+        "thumbnail": "img11.jpg",
+        "id": 1,
+    },
+    {
+        "title": "This is a Blog 2",
+        "subtitle": "This is subtitle of the blog",
+        "description": "This is the description of my new blog",
+        "thumbnail": "img11.jpg",
+        "id": 2,
+    },
+]
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", blogs=blogs)
 
 
-@app.route("/dashboard")
-def dash():
-    return render_template("dashboard.html")
+@app.route("/blog/<int:id>")
+def blog(id):
+    return f"<h1>This is Blog {id}</h1>"
 
 
 app.run(debug=True)
