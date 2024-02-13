2b1973b86219c62ddbdd10037e424e1d5e5d59c0
kamal Kumar <kamal.kumar9@outlook.com>
2024-02-13 22:40:08 +0530

    Added Login functionality

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from flask import Flask, redirect, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config['SECRET_KEY'] = 'my secret key'
 
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.session_protection = 'strong'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Profiles.query.get(int(user_id))
 
 
 class Blogs(db.Model):
@@ -14,6 +24,16 @@ class Blogs(db.Model):
     subtitle = db.Column(db.String(400), nullable=True)
     description = db.Column(db.Text, nullable=True)
     thumbnail = db.Column(db.String(180), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'profiles.id'), nullable=False)
+
+
+class Profiles(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), nullable=False)
+    name = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    blogs = db.relationship('Blogs', backref='author', lazy=True)
 
 
 with app.app_context():
@@ -32,6 +52,7 @@ def blog(blog_id):
 
 
 @app.route("/blog/create", methods=["GET", "POST"])
+@login_required
 def create_blog():
     if request.method == "POST":
         blog = Blogs(
@@ -68,6 +89,40 @@ def delete_blog(blog_id):
         db.session.commit()
         return redirect(url_for("home"))
     return "<h1>404: Blog not found</h1>"
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        if request.form.get("password") != request.form.get("cnf_password"):
+            return '<h1>Passwords are not matching.</h1>'
+        user = Profiles(
+            email=request.form.get("email"),
+            name=request.form.get("name"),
+            password=request.form.get("password")
+        )
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return redirect(url_for('home'))
+    return render_template('signup.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = Profiles.query.filter_by(email=request.form.get('email')).first()
+        if user and (user.password == request.form.get('password')):
+            login_user(user)
+        else:
+            return '<h1>Incorrect Credentials</h1>'
+        return redirect(url_for('home'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 app.run(debug=True)
