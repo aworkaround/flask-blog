421ea074c76bbba3308e9ba8f7ff257713c52119
kamal Kumar <kamal.kumar9@outlook.com>
2024-03-29 17:33:52 +0530

    Deploy

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABSE_URL')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
 app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
